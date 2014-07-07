6feb1f5f96db8e97eee994831f1f79ce075a6356
csb324 <csb324@gmail.com>
2014-07-07 18:05:26 -0400

    The first test passes!!!
    * A user can sign in
    * When the user goes to the root, phe sees a list of all the locations!

--- spec/features/user_views_locations_spec.rb
+++ spec/features/user_views_locations_spec.rb
@@ -4,11 +4,15 @@ feature 'User views locations' do
   scenario 'and they all appear' do
 
     carb = create(:user, last_name: 'carburry')
+    ratty = create(:location, name: "The Ratty")
+    jos = create(:location, name: "Jo's")
     sign_in_as(carb)
 
     visit root_path
+    save_and_open_page
 
     expect(page).to have_content "The Ratty"
+    expect(page).to have_content "Jo's"
 
   end
 end
