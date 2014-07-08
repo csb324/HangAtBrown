8440705a1d7281bdd7a2a426e3cec41c4aab118a
csb324 <csb324@gmail.com>
2014-07-07 21:03:19 -0400

    Tests to make sure events in the past don't appear

--- spec/features/user_views_events_spec.rb
+++ spec/features/user_views_events_spec.rb
@@ -10,8 +10,8 @@ feature 'User views one location' do
 
     ratty = create(:location, name: "The Ratty")
     jos = create(:location, name: "Jo's")
-    ev1 = create(:event, location: ratty, topic: "Burgers")
-    ev1r = create(:rsvp, user: ruth, event: ev1, creator: true)
+    event_one = create(:event, location: ratty, topic: "Burgers")
+    event_oner = create(:rsvp, user: ruth, event: event_one, creator: true)
 
     visit root_path
 
@@ -21,7 +21,29 @@ feature 'User views one location' do
 
   end
 
-  # scenario 'and events in the past do not appear' do
-  # end
+  scenario 'and events in the past do not appear' do
+
+    carb = create(:user)
+    ruth = create(:user, first_name: 'Ruth')
+    ratty = create(:location, name: "The Ratty")
+    sign_in_as(carb)
+
+    event_two = create(:event, location: ratty, topic: "Physics", start_time: Time.now - 1.day, end_time: Time.now - 16.hours)
+    event_two_rsvp = create(:rsvp, user: ruth, event: event_two, creator: true)
+
+    event_one = create(:event, location: ratty, topic: "Burgers")
+    event_one_rsvp = create(:rsvp, user: ruth, event: event_one, creator: true)
+
+    visit root_path
+
+    click_link "The Ratty"
+
+    save_and_open_page
+
+    expect(page).to have_content "Burgers"
+    expect(page).to have_no_content "Physics"
+
+
+  end
 
 end
