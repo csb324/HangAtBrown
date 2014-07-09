656fbcfd6ab75d248c3c6410556a010f799db902
csb324 <csb324@gmail.com>
2014-07-08 20:19:37 -0400

    Authetication works now

--- DOING RIGHT NOW.md
+++ DOING RIGHT NOW.md
@@ -1,1 +1,1 @@
-DOING RIGHT NOW: user can update their interests, concentration, and class year
+DOING RIGHT NOW: authenticating
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,7 @@
 class LocationsController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
     @locations = Location.all
   end
