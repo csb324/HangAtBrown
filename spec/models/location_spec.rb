7d619dd68effb85328cc7f2c0c315d439e3a324a
csb324 <csb324@gmail.com>
2014-07-08 21:48:31 -0400

    Location knows about current events!

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -13,7 +13,7 @@ describe Location do
 
   describe '#events' do
     it 'has events' do
-      study = Event.new(location: rock, start_time: Time.now - 60.minutes , end_time: Time.now - 45.minutes, event_type: "study")
+      study = create(:event, location: rock)
       study.save(validate: false)
       expect(rock.events).to include(study)
     end
@@ -21,11 +21,19 @@ describe Location do
 
   describe '#current_events' do
     it 'does not have events that are in the past' do
-      study_morning = Event.new(location: rock, start_time: Time.now - 60.minutes , end_time: Time.now - 45.minutes, event_type: "study")
+      study_morning = build(:event, location: rock, start_time: Time.now - 2.hours, end_time: Time.now - 1.hour)
       study_morning.save(validate: false)
 
       expect(rock.current_events).not_to include(study_morning)
     end
+
+    it 'has events that are currently happening' do
+      study_afternoon = build(:event, location: rock, start_time: Time.now - 1.hour, end_time: Time.now + 1.hour)
+      study_afternoon.save(validate: false)
+      expect(rock.current_events).to include(study_afternoon)
+    end
+
   end
+
 
 end
