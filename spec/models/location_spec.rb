50aaaa9987cb26736506a1fc530b1cd47eb34a56
csb324 <csb324@gmail.com>
2014-07-08 21:59:48 -0400

    Made location spec prettier

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -2,37 +2,48 @@ require 'rails_helper'
 
 describe Location do
 
-  let(:rock) { Location.new(name: "The Rock", description: "It used to be called the John.", url: "http://www.jpeg.com/yes.jpg")}
+  before(:each) do
+    @rock = create(:location, name: "The Rock", description: "It's a library")
+
+    # Happening later
+    @study = create(:event, location: @rock)
+
+    # Happened earlier today
+    @study_morning = build(:event, location: @rock, start_time: Time.now - 2.hours, end_time: Time.now - 1.hour)
+    @study_morning.save(validate: false)
+
+    # Happpening now
+    @study_afternoon = build(:event, location: @rock, start_time: Time.now - 1.hour, end_time: Time.now + 1.hour)
+    @study_afternoon.save(validate: false)
+  end
 
   describe '#initialize' do
     it 'has a name and a description' do
-      expect(rock.name).to eq "The Rock"
-      expect(rock.description).to eq "It used to be called the John."
+      expect(@rock.name).to eq "The Rock"
+      expect(@rock.description).to eq "It's a library"
     end
   end
 
   describe '#events' do
     it 'has events' do
-      study = create(:event, location: rock)
-      study.save(validate: false)
-      expect(rock.events).to include(study)
+      expect(@rock.events).to include(@study)
+      expect(@rock.events).to include(@study_morning)
+      expect(@rock.events).to include(@study_afternoon)
     end
   end
 
   describe '#current_events' do
     it 'does not have events that are in the past' do
-      study_morning = build(:event, location: rock, start_time: Time.now - 2.hours, end_time: Time.now - 1.hour)
-      study_morning.save(validate: false)
-
-      expect(rock.current_events).not_to include(study_morning)
+      expect(@rock.current_events).not_to include(@study_morning)
     end
 
     it 'has events that are currently happening' do
-      study_afternoon = build(:event, location: rock, start_time: Time.now - 1.hour, end_time: Time.now + 1.hour)
-      study_afternoon.save(validate: false)
-      expect(rock.current_events).to include(study_afternoon)
+      expect(@rock.current_events).to include(@study_afternoon)
     end
 
+    it 'does not have events that are in the future' do
+      expect(@rock.current_events).not_to include(@study)
+    end
   end
 
 
